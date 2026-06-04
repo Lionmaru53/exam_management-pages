3de11f19c427d56f3a1a4f4f421922549c80c7c3
Lionmaru53 <Lionmaru53@users.noreply.github.com>
2026-06-04 03:45:31 +0000

    deploy: Lionmaru53/exam_management@bd557c7239a9c007a5d1b1d36792589eb085bc2b

---
+++ config.example.js
@@ -0,0 +1,5 @@
+// このファイルをコピーして config.js を作成し、実際の値を入力してください
+// config.js は .gitignore で管理外になっています
+
+const LIFF_ID = 'YOUR_LIFF_ID_HERE';         // 例: '1234567890-AbCdEfGh'
+const GAS_URL = 'YOUR_GAS_EXEC_URL_HERE';     // 例: 'https://script.google.com/macros/s/AKfycb.../exec'
---
+++ config.js
@@ -0,0 +1,2 @@
+const LIFF_ID = '2010067934-XK0RPTwa';
+const GAS_URL  = 'https://script.google.com/macros/s/AKfycbyz4MLhrFoP3W7a9FDRk9LP4IiExBVn7xvBHVMZHECr/dev';
